efec42dfadc84e742f34212b491ae44c27b4c45c
Tom Boettger <t.boettger@live.de>
2020-07-12 18:52:38 +0200

    Insert switch to show plain md (additionally fixes #5)

--- src/components/MarkdownOutput.vue
+++ src/components/MarkdownOutput.vue
@@ -1,21 +1,45 @@
 <template>
-  <div class="shadow rounded-lg p-2">
-        <b-tabs pills small>
-          <b-tab title="CONTRIBUTING.md" active>
-            <b-card-text>
-              <div class="d-flex mt-3">
-                <vue-markdown>{{ contributingMarkdown }}</vue-markdown>
-              </div>
-            </b-card-text>
-          </b-tab>
-          <b-tab title="CODE_OF_CONDUCT.md">
-            <b-card-text>
-              <div class="d-flex mt-3">
-                <vue-markdown>{{ codeOfConductMarkdown }}</vue-markdown>
-              </div>
-            </b-card-text>
-          </b-tab>
-        </b-tabs>
+  <div class="shadow rounded-lg p-2 mb-3">
+    <b-tabs pills small>
+      <b-tab title="CONTRIBUTING.md" active>
+        <div class="float-right">
+          <div class="d-flex">
+            <b-icon-info-circle v-b-tooltip.hover title="Show plain markdown" class="ml-auto"></b-icon-info-circle>
+            <b-form-checkbox
+              id="plainMarkdownSwitch"
+              switch
+              size="lg"
+              class="ml-1"
+              v-model="plainMarkdown"
+            ></b-form-checkbox>
+          </div>
+        </div>
+
+        <div class="inline text-left mt-3">
+          <vue-markdown v-show="!plainMarkdown">{{ contributingMarkdown }}</vue-markdown>
+          <p v-show="plainMarkdown">{{ contributingMarkdown }}</p>
+        </div>
+      </b-tab>
+      <b-tab title="CODE_OF_CONDUCT.md">
+        <div class="float-right">
+          <div class="d-flex">
+            <b-icon-info-circle v-b-tooltip.hover title="Show plain markdown" class="ml-auto"></b-icon-info-circle>
+            <b-form-checkbox
+              id="plainMarkdownSwitch"
+              switch
+              size="lg"
+              class="ml-1"
+              v-model="plainMarkdown"
+            ></b-form-checkbox>
+          </div>
+        </div>
+
+        <div class="inline text-left mt-3">
+          <vue-markdown v-show="!plainMarkdown">{{ codeOfConductMarkdown }}</vue-markdown>
+          <p v-show="plainMarkdown">{{ codeOfConductMarkdown }}</p>
+        </div>
+      </b-tab>
+    </b-tabs>
   </div>
 </template>
 
@@ -27,12 +51,21 @@ export default {
   components: {
     VueMarkdown
   },
-  props: ["contributingMarkdown", "codeOfConductMarkdown"]
+  props: ["contributingMarkdown", "codeOfConductMarkdown"],
+  data() {
+    return {
+      plainMarkdown: false
+    };
+  }
 };
 </script>
 
 <style scoped>
 div {
   background-color: var(--light);
+}
+.custom-switch {
+  top: -7px;
+  left: 5px;
 }
 </style>
