f2bb5f8a6a4855561373723a4f14cb046c7a8cc1
Seppe Volkaerts <seppevolkaerts@hotmail.com>
2020-07-13 01:18:48 +0200

    Add download buttons for the generated files. Closes #8

--- src/components/MarkdownOutput.vue
+++ src/components/MarkdownOutput.vue
@@ -1,7 +1,13 @@
 <template>
   <div id="mdOutputDiv" class="shadow rounded-lg p-2 mb-3">
     <b-tabs pills>
-      <b-tab v-for="tab in tabs" :key="tab.title" :title="tab.title">
+      <b-tab v-for="tab in tabs" :key="tab.title">
+        <template v-slot:title>
+          {{ tab.title }}
+          <b-button pill class="ml-2" @click.prevent="clickDownload($event, tab.title, tab.markdown)">
+            <b-icon icon="download"></b-icon>
+          </b-button>
+        </template>
         <InputSwitchMarkdown v-model="showPlainMarkdown" />
         <div class="inline text-left mt-3">
           <vue-markdown v-show="!showPlainMarkdown" v-bind:source="tab.markdown">{{ tab.markdown }}</vue-markdown>
@@ -27,6 +33,18 @@ export default {
     return {
       showPlainMarkdown: false
     };
+  },
+  methods: {
+    clickDownload(event, title, content) {
+      event.stopPropagation();
+
+      const blob = new Blob([content], { type: 'text/markdown; charset=UTF-8' })
+      const link = document.createElement('a')
+      link.href = URL.createObjectURL(blob)
+      link.download = title
+      link.click()
+      URL.revokeObjectURL(link.href)
+    }
   }
 };
 </script>
